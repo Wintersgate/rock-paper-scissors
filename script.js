fa54c3971b133529716e502eb394eec7780e2e79
Corey Graham <142066414+Wintersgate@users.noreply.github.com>
2024-04-16 22:18:12 -0500

    fixed single round function, added div

--- script.js
+++ script.js
@@ -44,7 +44,9 @@ function playGame () {
             } else if (button.id === 'scissors') {
                 playerSelection = 'scissors';
             }
-            console.log(playerSelection);
+            const computerSelection = getComputerChoice();
+            const result = playRound(playerSelection, computerSelection);
+            console.log(result);
         });
     });
 }
